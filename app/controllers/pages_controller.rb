0072295c1a7c07d510855d51d269f8e0307e9a93
Damian Mastylo <dmastylo@gmail.com>
2014-02-05 01:49:21 -0500

    Change to Bootstrap 3 markup, and some minor code cleanup, much more to do

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,22 +1,22 @@
 class PagesController < ApplicationController
-    before_filter :user_signed_in?
+  before_filter :signed_in?
 
-    def home
-        if user_signed_in?
-            @projects = Project.sorted_by_recent_work
+  def home
+    if signed_in?
+      @projects = Project.sorted_by_recent_work
 
-            # Initial setup for first project
-            @project = Project.new
+      # Initial setup for first project
+      @project = Project.new
 
-            if !@projects.last.nil?
-                @existing_task = current_user.has_running_task?
+      unless @projects.last.nil?
+        @existing_task = current_user.has_running_task?
 
-                if @existing_task
-                    @time_spent = current_user.time_spents.last
-                else
-                    @time_spent = current_user.time_spents.build
-                end
-            end
+        if @existing_task
+          @time_spent = current_user.time_spents.last
+        else
+          @time_spent = current_user.time_spents.build
         end
+      end
     end
+  end
 end
