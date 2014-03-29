acc125e90ee446fd21055f64f28383c3f7e731db
Damian Mastylo <dmastylo@gmail.com>
2014-03-29 04:57:48 -0400

    Hourly rate on projects, with all logic for correct form redisplays

--- app/models/project.rb
+++ app/models/project.rb
@@ -25,6 +25,7 @@ class Project < ActiveRecord::Base
   validates :description, presence: true
   validates :privacy_type, presence: true, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 2, message: "is an invalid privacy type" }
   validates :creator_id, presence: true
+  validates :hourly_rate_cents, presence: true, if: :billable
 
   # Relationships
   # ========================================================
@@ -35,7 +36,7 @@ class Project < ActiveRecord::Base
 
   # Money
   # ========================================================
-  monetize :hourly_rate_cents, numericality: { greater_than_or_equal_to: 0 }
+  monetize :hourly_rate_cents, numericality: { greater_than: 0 }, allow_nil: true
 
   # Constants
   # ========================================================
